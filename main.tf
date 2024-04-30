778eaa8f319de08c01452e2025d77d84fa3297d5
Olalomi Safiu <olalomisafiu@Olalomis-MBP.lan>
2024-04-29 18:06:53 -0600

    new test

--- main.tf
+++ main.tf
@@ -2,6 +2,12 @@ resource "aws_iam_virtual_mfa_device" "example" {
 virtual_mfa_device_name = "example"
 }
 
+resource "hcp_service_principal" "sp" {
+name   = "example-sp"
+parent = hcp_project.example.resource_name
+}
 
-
-
+resource "hcp_service_principal" "sp" {
+name   = "example-sp"
+parent = hcp_project.example.resource_name
+}
