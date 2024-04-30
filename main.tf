659020be719035d24cfc902c4e385768bf38f80a
Olalomi Safiu <olalomisafiu@Olalomis-MBP.lan>
2024-04-30 09:28:27 -0600

    test 2

--- main.tf
+++ main.tf
@@ -10,3 +10,7 @@ resource "aws_iam_virtual_mfa_device" "example" {
   virtual_mfa_device_name = "example"
 }
 
+resource "aws_iam_virtual_mfa_device" "example" {
+virtual_mfa_device_name = "example"
+}
+
