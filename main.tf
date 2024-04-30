3c4a9c572efd57385c166cee714e75e6c4d4b60f
Olalomi Safiu <olalomisafiu@Olalomis-MBP.lan>
2024-04-29 18:06:20 -0600

    rubbish

--- main.tf
+++ main.tf
@@ -1,2 +1,7 @@
-resource "aws_iam_virtual_mfa_device" "example" {virtual_mfa_device_name = "example"
+resource "aws_iam_virtual_mfa_device" "example" {
+virtual_mfa_device_name = "example"
 }
+
+
+
+
