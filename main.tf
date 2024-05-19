b59954069f04246a1757b64bcc446f47881f76c4
Olalomi Safiu <olalomisafiu@Olalomis-MacBook-Pro.local>
2024-05-19 17:06:25 -0500

    run test

--- main.tf
+++ main.tf
@@ -3,5 +3,11 @@ virtual_mfa_device_name = "example"
 }
 
 
+resource "aws_iam_virtual_mfa_device" "example" {
+virtual_mfa_device_name = "example"
+}
 
+resource "aws_iam_virtual_mfa_device" "example" {
+virtual_mfa_device_name = "example"
+}
 
