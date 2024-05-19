882462286d7184a6a6fc046baa48c9c307d9317d
Olalomi Safiu <olalomisafiu@Olalomis-MacBook-Pro.local>
2024-05-19 16:51:16 -0500

    git commit test

--- main.tf
+++ main.tf
@@ -2,6 +2,10 @@ resource "aws_iam_virtual_mfa_device" "example" {
 virtual_mfa_device_name = "example"
 }
 
+resource "aws_iam_virtual_mfa_device" "example" {
+virtual_mfa_device_name = "example"
+}
 
-
-
+resource "aws_iam_virtual_mfa_device" "example" {
+virtual_mfa_device_name = "example"
+}
